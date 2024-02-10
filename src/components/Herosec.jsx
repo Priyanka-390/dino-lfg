2d4514560bdea68386bce9e9ccf1aa0bb39fd010
Priyanka-390 <psr82220@gmail.com>
2024-02-10 14:23:44 +0530

    solve errors

--- src/components/Herosec.jsx
+++ src/components/Herosec.jsx
@@ -11,21 +11,21 @@ const Herosec = () => {
 
           <div
             data-aos="fade-left"
-            class="sm:w-7/12 w-full flex justify-center aos-init aos-animate"
+            className="sm:w-7/12 w-full flex justify-center aos-init aos-animate"
           >
-            <h1 class="ff-lucky font-normal lg:text-[175.64px] md:text-[130px] sm:text-[110px] text-[75px] leading-[91%] text-center drop-shadow-[0px_6px_0px_#000]">
-              <span class="text-[#FBA11D]">D</span>
-              <span class="text-[#23AAAC]">I</span>
-              <span class="text-[#D45B07]">N</span>
-              <span class="text-[#8EC627]">O</span>
-              <br class="sm:block hidden" />
-              <span class="text-[#8EC627] drop-shadow-[0px_2px_0px_#000]">
+            <h1 className="ff-lucky font-normal lg:text-[175.64px] md:text-[130px] sm:text-[110px] text-[75px] leading-[91%] text-center drop-shadow-[0px_6px_0px_#000]">
+              <span className="text-[#FBA11D]">D</span>
+              <span className="text-[#23AAAC]">I</span>
+              <span className="text-[#D45B07]">N</span>
+              <span className="text-[#8EC627]">O</span>
+              <br className="sm:block hidden" />
+              <span className="text-[#8EC627] drop-shadow-[0px_2px_0px_#000]">
                 L
               </span>
-              <span class="text-[#FCCA2B] drop-shadow-[0px_2px_0px_#000]">
+              <span className="text-[#FCCA2B] drop-shadow-[0px_2px_0px_#000]">
                 F
               </span>
-              <span class="text-[#8F78D2] drop-shadow-[0px_2px_0px_#000]">
+              <span className="text-[#8F78D2] drop-shadow-[0px_2px_0px_#000]">
                 G
               </span>
             </h1>
